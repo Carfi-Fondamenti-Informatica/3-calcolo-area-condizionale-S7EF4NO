1c6d960b1b481b954ccb90e7c552c1164f87b346
S7EF4NO <100076214+S7EF4NO@users.noreply.github.com>
2022-03-29 18:48:27 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,26 @@
 using namespace std;
 
 int main() {
-   return 0;
+float a=0,b=0,area=0;
+int opzione=0;
+
+cin>>a>>b;
+cin>> opzione;
+
+switch (opzione) {
+    case 0:
+        area = (a * b) / 2;
+        break;
+    case 1:
+        area = a * a;
+        break;
+    case 2:
+        area = a * b;
+        break;
+    default:
+        cout << "opzione non valida" << endl;
+}
+cout<< area<< endl;
+
+    return 0;
 }
